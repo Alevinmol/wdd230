068ba683f8c45e59e960c73771ad07f9359ce7f0
Alevinmol <vinay2@byui.edu>
2023-11-26 23:06:14 -0600

    final

--- chamber/scripts/script.js
+++ chamber/scripts/script.js
@@ -17,6 +17,37 @@ if (today >= 1 && today <= 3) {
 }
 
 function closeBanner() {
-    // Close the banner when the close button is clicked
     document.getElementById('event-banner').style.display = 'none';
 }
+
+//spotlight script//
+async function fetchJSONData() {
+    try {
+        const response = await fetch('https://alevinmol.github.io/wdd230/chamber/data/members.json');
+        const jsonData = await response.json();
+
+        if (jsonData && jsonData.members) {
+            const silverGoldMembers = jsonData.members.filter(member => member.membership === 'Silver' || member.membership === 'Gold');
+
+            function shuffleArray(array) {
+                for (let i = array.length - 1; i > 0; i--) {
+                    const j = Math.floor(Math.random() * (i + 1));
+                    [array[i], array[j]] = [array[j], array[i]];
+                }
+            }
+
+            shuffleArray(silverGoldMembers);
+            const spotlightList = document.getElementById('spotlight-list');
+            for (let i = 0; i < Math.min(3, silverGoldMembers.length); i++) {
+                const member = silverGoldMembers[i];
+                const listItem = document.createElement('li');
+                listItem.textContent = `${member.name} - ${member.membership} Member`;
+                spotlightList.appendChild(listItem);
+            }
+        }
+    } catch (error) {
+        console.error('Error fetching JSON data:', error);
+    }
+}
+
+fetchJSONData();
